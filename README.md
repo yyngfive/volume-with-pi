af1628c4fc15a9f5ad34bdd40b1dbd14a051df85
yyngfive <chenhye5@outlook.com>
2022-02-19 11:21:19 +0800

    考虑相对运动，实现了显示效果，但是tkinter有点难用，写起来不是很方便

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ![参考](/image/pi.gif)
 
 [原文在这里](https://www.reddit.com/r/ProgrammerHumor/comments/6f7dr9/volume_in_digits_of_pi/)
+
+然而我没有找到程序从哪里下载，只好自己简单实现了一下并做了一些简单分析。
--- src/volume_with_pi.py
+++ src/volume_with_pi.py
@@ -1,30 +1,48 @@
 import tkinter as tk
+from icecream import ic
+
+#TODO: 优化代码结构
 
 root = tk.Tk()
 screen_w = root.winfo_screenwidth()
 screen_h = root.winfo_screenheight()
-window_w = 250
-window_h = 100
+window_w = 500
+window_h = 120
 root.geometry(f'{window_w}x{window_h}+' + 
               f'{int((screen_w-window_w)/2)}+{int((screen_h-window_h)/2)}')
 root.resizable(0,0)
 label_volume = tk.Label(root,text = 'Volume')
 pi_61 = '31415926535897932384626433832795028841971693993751058209749445'
 pi = pi_61
-current = tk.StringVar()
-label_pi = tk.Label(root,text = pi,font={'Arial',10})
-label_current = tk.Label(root,textvariable=current,bg='white',width=10)
-btn_last = tk.Button(root,text = 'Last')
-btn_next = tk.Button(root,text='Next')
-check_mute = tk.Checkbutton(root,text='Mute')
+pi_current = tk.StringVar()
+pi_previous = tk.StringVar()
+pi_next = tk.StringVar()
+muted = tk.BooleanVar()
+pi_x = 0
+frame_pi = tk.Frame(root)
+frame_btn = tk.Frame(root)
+label_previous = tk.Label(frame_pi,textvariable=pi_previous)
+label_current = tk.Label(frame_pi,textvariable=pi_current,bg='white',width=2)
+label_next = tk.Label(frame_pi,textvariable = pi_next)
+btn_last = tk.Button(frame_btn,text = 'Previous')
+btn_next = tk.Button(frame_btn,text='Next')
 
+#TODO: 实现静音功能
+#受限于所取圆周率位数，找不到字符串“00”，无法实现静音
+def mute():
+    ic(str(muted.get()))
 
-def current_volumne():
-    pass
+check_mute = tk.Checkbutton(frame_btn,text='Mute',var = muted,command=mute)
 
-def current_pi():
+#TODO：获取当前音量
+def volume_current():
     return 10
 
+#TODO 依据音量寻找61位圆周率中最接近的值，返回第一个位置
+def volume_to_pi(volume):
+    return 10
+
+#TODO：增加音量设置
 def set_volume(value):
     pass
 
@@ -36,42 +54,65 @@ def pi_to_volume(location):
     else:
         return -1
 
-pi_x = current_pi()
-current.set(pi_to_volume(pi_x))
+
 
 def pi_split(location):
     previous = pi[0:location]
     last = pi[location+2:]
     return (previous,last)
 
+def pi_show(previous,current,_next):
+    pi_previous.set(previous)
+    pi_current.set(current)
+    pi_next.set(_next)
+
 def go_last(event):
     global pi_x
     pi_x -= 1
-    print(pi_to_volume(pi_x))
+    ic(pi_to_volume(pi_x))
     volume = pi_to_volume(pi_x)
     if volume != -1:
         volume = pi_to_volume(pi_x)
         set_volume(volume)
-        current.set(volume)
+        previous,_next = pi_split(pi_x)
+        pi_show(previous,volume,_next)
 def go_next(event):
     global pi_x
     pi_x += 1
-    print(pi_to_volume(pi_x))
+    ic(pi_to_volume(pi_x))
     volume = pi_to_volume(pi_x)
     if volume != -1:
         volume = pi_to_volume(pi_x)
         set_volume(volume)
-        current.set(volume)
+        previous,_next = pi_split(pi_x)
+        pi_show(previous,volume,_next)
+
+def display():
+    volume = pi_to_volume(pi_x)
+    if volume != -1:
+        volume = pi_to_volume(pi_x)
+        set_volume(volume)
+        previous,_next = pi_split(pi_x)
+        pi_show(previous,volume,_next)
+
+def init():
+    global pi_x
+    pi_x = volume_to_pi(volume_current())
+    pi_current.set(pi_to_volume(pi_x))
+    display()
+    
     
 
-
-
+init()
 label_volume.pack()
-label_pi.place(x = pi_x,y = 20)
-label_current.pack()
+label_previous.pack(side='left',expand = True)
+label_current.pack(side='left',expand = True)
+label_next.pack(side='left',expand = True)
 btn_last.pack(side='left',expand = True)
 btn_last.bind('<Button-1>',go_last)
 check_mute.pack(side='left',expand = True)
 btn_next.pack(side='left',expand=True)
 btn_next.bind('<Button-1>',go_next)
+frame_pi.pack()
+frame_btn.pack()
 root.mainloop()
